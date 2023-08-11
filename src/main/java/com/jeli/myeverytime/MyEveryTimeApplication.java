acac18b4a3243093e1634aca998e98cb0b20fb4d
jeli01 <jeli01@naver.com>
2023-08-12 02:56:48 +0900

    initial commit

---
+++ src/main/java/com/jeli/myeverytime/MyEveryTimeApplication.java
@@ -0,0 +1,13 @@
+package com.jeli.myeverytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEveryTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEveryTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeli/myeverytime/MyEveryTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeli.myeverytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEveryTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
